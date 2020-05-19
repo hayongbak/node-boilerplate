840d415771a4fca18f34771db91ee1d2de595df3
ultrafast1226 <47691898+ultrafast1226@users.noreply.github.com>
2020-05-19 16:10:35 -0300

    Update dev.config.js

--- app/config/dev.config.js
+++ app/config/dev.config.js
@@ -1,5 +1,5 @@
 
 module.exports = {
-  mongoURI: "mongodb+srv://kingsdaniel285:gold.1226P@cluster0-fagyc.mongodb.net/Database?retryWrites=true",
+  mongoURI: "mongodb+srv://<username>:<password>@cluster0-fagyc.mongodb.net/Database?retryWrites=true",
   port: 3001
 };
